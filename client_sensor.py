1c35e4bc3fbbe2731818aa451fbff8cd124d8b25
Pavlo Hilei <pavlo.hilei@gmail.com>
2025-02-16 20:12:16 +0100

    Update client code

--- client_sensor.py
+++ client_sensor.py
@@ -1,15 +1,15 @@
 import requests
 
-SERVER_IP = 'SERVER_IP_ADDRESS'
+SERVER_IP = '192.168.135.13'
 PORT = 5000
 BASE_URL = f'http://{SERVER_IP}:{PORT}'
 
-def set_value(value):
-    response = requests.post(f'{BASE_URL}/set_value', json={'value': value})
+def set_servo_angle(value):
+    response = requests.post(f'{BASE_URL}/set_servo_angle', json={'value': value})
     print('set_value response:', response.json())
 
-def get_value():
-    response = requests.get(f'{BASE_URL}/get_value')
+def get_servo_angle():
+    response = requests.get(f'{BASE_URL}/get_servo_angle')
     print('get_value response:', response.json())
 
 def check_alive():
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     while True:
         angle = input("Angle: ")
         angle = int(angle)
-        set_value(angle)
+        set_servo_angle(angle)
         
-        get_value()
+        get_servo_angle()
         check_alive()
